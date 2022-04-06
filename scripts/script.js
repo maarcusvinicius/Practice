723af41bd67b6028462142ae5c37f2fa9f789125
Marcus Vinicius <marcus.editor77@gmail.com>
2022-04-06 11:35:29 -0300

    proj 1 atl 6

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
-let inputPrimeiroNumber = document.getElementById("PrimeiroCampo");
-let inputSegundoNumber = document.getElementById("SegundoCampo");
+let InputCampoUm = document.getElementById("PrimeiroCampo");
+let InputCampoDois = document.getElementById("SegundoCampo");
 
 let resultado = document.getElementById("resultado");
 
 function calcular() {
   console.log("Calculando...");
-  
-  
+
+  let larguraDaParede = inputPrimeiroNumber.value;
+  let alturaDaParede = inputSegundoNumber.value;
+
+  let area = larguraDaParede * alturaDaParede;
+  let tinta = area / 2;
+
+  console.log(area, tinta);
+
+  resultado.innerHTML = `<p>Sua parede tem a dimensão de ${larguraDaParede}x${alturaDaParede} e sua área é de ${area}m²</p>`                      
+  resultado.innerHTML += `<p>Para pintar essa parede, você presisará de ${tinta}L de tinta</p>` 
 }
