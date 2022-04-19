387f21897f5f9b8abc2d49ce5c5c4be3536c5666
Marcus Vinicius <marcus.editor77@gmail.com>
2022-04-19 11:43:39 -0300

    proj 1 atl 8

--- scripts/script.js
+++ scripts/script.js
@@ -3,23 +3,29 @@ let InputCampoDois = document.getElementById("SegundoCampo");
 
 let resultado = document.getElementById("resultado");
 
-function calcular() { 
-  botaoHandler(event);
-  function botaoHandler(event){
-    event.preventDefault();
-    console.log("Botão Clicado!");
+let chek = document.getElementById("card1");
+
+if (document.getElementById("botao").onclick == false) {
+  chek.style.visibility = 'hidden';
+} else {
+  function calcular() {
+    botaoHandler();
+    function botaoHandler() {
+      event.preventDefault();
+      console.log("Botão Clicado!");
+    }
+
+    console.log("Calculando...");
+
+    let larguraDaParede = InputCampoUm.value;
+    let alturaDaParede = InputCampoDois.value;
+
+    let area = larguraDaParede * alturaDaParede;
+    let tinta = area / 2;
+
+    console.log(area, tinta);
+
+    resultado.innerHTML = `<p>Sua parede tem a dimensão de ${larguraDaParede}x${alturaDaParede} e sua área é de ${area}m²</p>`;
+    resultado.innerHTML += `<p>Para pintar essa parede, você presisará de ${tinta}L de tinta</p>`;
   }
-
-  console.log("Calculando...");
-
-  let larguraDaParede = InputCampoUm.value;
-  let alturaDaParede = InputCampoDois.value;
-
-  let area = larguraDaParede * alturaDaParede;
-  let tinta = area / 2;
-
-  console.log(area, tinta);
-
-  resultado.innerHTML = `<p>Sua parede tem a dimensão de ${larguraDaParede}x${alturaDaParede} e sua área é de ${area}m²</p>`                      
-  resultado.innerHTML += `<p>Para pintar essa parede, você presisará de ${tinta}L de tinta</p>` 
 }
