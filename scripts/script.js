d362b382174ecbe213611af405b2d4659f3652b9
Marcus Vinicius <marcus.editor77@gmail.com>
2022-04-20 10:34:15 -0300

    proj 1 atl 9

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,38 @@
-let InputCampoUm = document.getElementById("PrimeiroCampo");
-let InputCampoDois = document.getElementById("SegundoCampo");
+var InputCampoUm = document.getElementById("PrimeiroCampo");
+var InputCampoDois = document.getElementById("SegundoCampo");
 
-let resultado = document.getElementById("resultado");
+var resultado = document.getElementById("resultado");
 
-let chek = document.getElementById("card1");
+var card = document.getElementById("card1");
+var enviar = document.getElementById("botao");
 
-if (document.getElementById("botao").onclick == false) {
-  chek.style.visibility = 'hidden';
-} else {
-  function calcular() {
-    botaoHandler();
-    function botaoHandler() {
-      event.preventDefault();
-      console.log("Botão Clicado!");
-    }
+var check = document.getElementById("confirm");
+card.style.visibility = "hidden";
 
-    console.log("Calculando...");
-
-    let larguraDaParede = InputCampoUm.value;
-    let alturaDaParede = InputCampoDois.value;
-
-    let area = larguraDaParede * alturaDaParede;
-    let tinta = area / 2;
-
-    console.log(area, tinta);
-
-    resultado.innerHTML = `<p>Sua parede tem a dimensão de ${larguraDaParede}x${alturaDaParede} e sua área é de ${area}m²</p>`;
-    resultado.innerHTML += `<p>Para pintar essa parede, você presisará de ${tinta}L de tinta</p>`;
+function v () {
+  botaoHandler();
+  function botaoHandler() {
+    event.preventDefault();
+    console.log("Botão Clicado!");
   }
+if (check.checked) {
+  card.style.visibility = "visible";
+  calcular();
+} else {
+  console.log("Algo deu errado");
+}
+}
+function calcular() {
+  console.log("Calculando...");
+
+  let larguraDaParede = InputCampoUm.value;
+  let alturaDaParede = InputCampoDois.value;
+
+  let area = larguraDaParede * alturaDaParede;
+  let tinta = area / 2;
+
+  console.log(area, tinta);
+
+  resultado.innerHTML = `<p>Sua parede tem a dimensão de ${larguraDaParede}x${alturaDaParede} e sua área é de ${area}m²</p>`;
+  resultado.innerHTML += `<p>Para pintar essa parede, você presisará de ${tinta}L de tinta</p>`;
 }
