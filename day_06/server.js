b4ae209e18c4bc464310287c314302ffadecc76a
maarcusvinicius <marcus.editor77@gmail.com>
2023-03-01 14:04:25 -0300

    challenge day 06 age with year

--- day_06/server.js
+++ day_06/server.js
@@ -1,7 +1,20 @@
-var firstName = "Marcus";
 
-var secondName = "Vinicius";
+function myFunction(years) {
+  const x = years;
+  const d = new Date();
+  age = d.getFullYear();
+  
+  acess = age - x
+  
+  if ( acess >= 18 ) {
+    console.log("Acesso liberado :)");
+  } else {
+    console.log("Acesso negado :(");
+  }
 
-var fullName = firstName + " " + secondName;
+  return (
+    "Ele(a) tem " + acess + " anos e nasceu em " + x
+  )
+}
 
-console.log(fullName)
+console.log(myFunction(2003));
