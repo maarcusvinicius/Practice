1d5d85332e5fc4fd6815f8ce201bc6e9c760e54a
maarcusvinicius <marcus.editor77@gmail.com>
2023-03-02 12:09:28 -0300

    day_06 new challenge

--- day_06/server.js
+++ day_06/server.js
@@ -1,20 +1,19 @@
-
-function myFunction(years) {
-  const x = years;
-  const d = new Date();
-  age = d.getFullYear();
-  
-  acess = age - x
-  
-  if ( acess >= 18 ) {
-    console.log("Acesso liberado :)");
-  } else {
-    console.log("Acesso negado :(");
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
   }
-
-  return (
-    "Ele(a) tem " + acess + " anos e nasceu em " + x
-  )
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-console.log(myFunction(2003));
+function main() {
+  const primeNumbers = Array.from({ length: 1000 }, (_, i) => i + 1)
+    .filter(isPrime);
+  return primeNumbers.reduce((acc, val) => acc + val, 0);
+}
+
+console.log(main());
