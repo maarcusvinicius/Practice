e532682f25e91f02285c8850afaa9e0eec77c13c
maarcusvinicius <marcus.editor77@gmail.com>
2023-04-27 23:02:07 -0300

    day_07 faild name and age

--- day_07/routes/student.js
+++ day_07/routes/student.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const students = [];
 
 router.get("/students", (req, res) => {
-  res.json(students);
+  // res.json(students);
+  res.json(JSON.stringify(students));
   console.log(students);
 });
 
